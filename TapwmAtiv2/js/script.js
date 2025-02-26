45fd56eae2ec90d8959b74b38c6dbfffea9c9398
Karina de Moraes Garcia <165072480+KarinaMoraesFatec@users.noreply.github.com>
2025-02-25 21:43:22 -0300

    Add files via upload

---
+++ TapwmAtiv2/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll("nav ul li a").forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
