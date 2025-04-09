cc0ccf418bf14ccaf745cd046f4464e4ee86d1b0
Karina de Moraes Garcia <165072480+KarinaMoraesFatec@users.noreply.github.com>
2025-04-09 16:06:07 -0300

    Delete Atividade4TAPWM/Atividade4TAPWM directory

--- Atividade4TAPWM/Atividade4TAPWM/Media.js
+++
@@ -1,8 +0,0 @@
-let nome = prompt("Qual é o nome do aluno?");
-
-let primeiraNota = prompt("Primeira nota:")
-let segundaNota = prompt("Segunda nota:")
-let terceiraNota = prompt("Terceira nota:")
-
-let media = (parseFloat(primeiraNota) + parseFloat(segundaNota) + parseFloat(terceiraNota)) / 3; 
-alert("Aluno:"+ nome+"\n Média: " + media +".");
